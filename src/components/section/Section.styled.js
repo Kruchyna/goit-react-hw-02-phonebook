cada0065031bbd74819481bed8f3b5b27b62e9c7
Kruchyna Elena <kruchynaelena@gmail.com>
2022-01-11 13:31:18 +0200

    files project

---
+++ src/components/section/Section.styled.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const SectionStyled = styled.div`
+  padding-top: 10px;
+  padding-bottom: 20px;
+`;
+
+export const SectionTitle = styled.h1`
+  margin-bottom: 20px;
+  font-size: ${({ hLevel }) => {
+    if (hLevel === "h1") {
+      return "25px";
+    }
+    if (hLevel === "h2") {
+      return "25px";
+    }
+  }};
+  ${({ visuallyHidden }) =>
+    visuallyHidden &&
+    `
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    border: 0;
+    padding: 0;
+    white-space: nowrap;
+    clip-path: inset(100%);
+    clip: rect(0 0 0 0);
+    overflow: hidden;
+  `}
+`;
